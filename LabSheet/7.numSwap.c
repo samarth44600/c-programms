6b0d3bdf72f78dce73af1ab279bc95ec6ca97f61
Samarth Subedi <subedi42samarth@gmail.com>
2023-01-31 22:52:10 +0545

    7 and 8 refactored

--- LabSheet/7.numSwap.c
+++ LabSheet/7.numSwap.c
@@ -2,10 +2,16 @@
 #include<conio.h>
 void main()
 {
-    int x = 10, y = 15, temp;
+    int x, y, temp;
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+    printf("Enter value of y: ");
+    scanf("%d", &y);
+    printf("Before swapping, x = %d and y = %d", x, y);
     temp = x;
     x = y;
     y = temp;
-    printf("x = %d and y = %d", x, y);
+    printf("\nAfter swapping, x = %d and y = %d", x, y);
+
     getch();
 }
--- LabSheet/8loop.c
+++ LabSheet/8loop.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
     /* 
         Always declare the variables before using them 
     */
     int i = 0;  // declaration and initialization at the same time
-    for(i = 0; i < 10; i++)
+    for(i = 0; i < 5; i++)
     {
         printf("i = %d\n", i);
 
@@ -13,9 +14,9 @@ int main()
 consequently, when i equals 10, the loop breaks. i is updated before the condition is checked- hence the value of i after exiting the loop is 10 
         */
      }
-    printf("\n\The value of i after exiting the loop is %d\n\n", i);
-    printf("\nRemember that the loop condition checks the conditional statement before it loops again.\n\n");
-    printf("Consequently, when i equals 10, the loop breaks.\n\n");
-    printf("i is updated before the condition is checked- hence the value of i after exiting the loop is 10 .\n\n");
-    return 0;
+    printf("\n\The value of i after exiting the loop is %d", i);
+    printf("\n For loop condition checks the conditional statement before it loops again.");
+    printf("\nConsequently, when i equals 10, the loop breaks.");
+    printf("\ni is updated before the condition is checked- hence the value of i after exiting the loop is 10 .");
+    getch();
 }
