4692bc0d8c313df52a65e494f16e83e5d44ff580
Samarth Subedi <subedi42samarth@gmail.com>
2023-01-31 20:01:29 +0545

    added 18 college programmes

---
+++ LabSheet/10whileLoop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    /* 
+        always declare the variables before using them 
+    */
+    int i = 10;     // declaration and initialization at the same time
+
+    do // do contains the actual code and the updation
+    {
+        printf("i = %d\n",i);
+        i = i-1;    // updation
+    }
+    // while loop doesn't contain any code but just the condition
+    while(i > 0);
+    
+    printf("\n\The value of i after exiting the loop is %d\n\n", i);
+    return 0;
+}
